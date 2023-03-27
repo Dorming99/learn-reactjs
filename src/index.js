6af22f72291898ea798d0d75a67ab2a7a2cb547c
Phuoc Minh <daophuocminh1@gmail.com>
2023-03-27 14:44:28 +0700

    edit1

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Hello wordld</h1>
-  </React.StrictMode>
+  <h1>Phuoc Minh</h1>
 );
 
 // If you want to start measuring performance in your app, pass a function
